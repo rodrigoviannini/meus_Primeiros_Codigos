4dfd8735b4f036a9c1880403c2a0ec0bb02149ab
Rodrigo Viannini <87579862+rodrigoviannini@users.noreply.github.com>
2021-07-20 23:06:35 -0300

    modificação enunciado

--- 003 - Exercicios Loop - While.py/005 - Progressão Geométrica  .py
+++ 003 - Exercicios Loop - While.py/005 - Progressão Geométrica  .py
@@ -1,13 +1,13 @@
-"""Progressão Geométrica de: 1/1 + 1/2 + 1/3 ... 1/1000"""
+"""Progressão Geométrica de: 1/1 + 1/2 + 1/4 + 1/8 + 1/16 + 1/32 ... 2"""
 # Sempre será igual a 2
 
 somatoria = 0
 contador = 1
 fator = 1
 
-while contador <= 1000:
+while contador < 1000:
     somatoria += 1/fator
     fator *= 2
     contador += 1
 
-print("Somatoria", round(somatoria, 3))
+print("PG", round(somatoria, 3))
