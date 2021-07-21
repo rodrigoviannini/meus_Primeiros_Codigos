1372d79ec0be45e3b58fb6eb1165608c2c6244cf
Rodrigo Viannini <87579862+rodrigoviannini@users.noreply.github.com>
2021-07-21 14:15:47 -0300

    incl ex009

---
+++ 004 - Exercicios Listas.py/009 - Sorted Decrescente.py
@@ -0,0 +1,7 @@
+lista = [36, 2, 15, 8, -3]
+
+lista_ordenada = sorted(lista)
+
+print(f'Lista em ordem crescente: {lista_ordenada}')
+
+print(f"Lista em ordem decrescente: {sorted(lista, reverse=True)}")
