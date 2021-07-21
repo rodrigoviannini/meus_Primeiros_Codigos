62b8abe52995cbe1b129e1e50c0f53b7a68e4c2d
Rodrigo Viannini <87579862+rodrigoviannini@users.noreply.github.com>
2021-07-21 19:44:01 -0300

    incl ex 003

---
+++ 001 - Exercicios Condicionais.py/003 - Funcao len.py
@@ -0,0 +1,8 @@
+def lenght(string):
+    pass # Pass ->  ignora o erro, da função com identação incompleta
+
+    tamanho = 0
+    for letra in string:
+        tamanho += 1
+    return tamanho 
+print(lenght("Quantos caracteres tem essa frase? "))
---
+++ 006 - Strings.py/002 - Percorrer str.py
@@ -0,0 +1,3 @@
+nome = input("Digite um nome: ")
+for i in range(len(nome)):
+    print(i,nome[i])
