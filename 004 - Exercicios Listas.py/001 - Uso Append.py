9c0bb6864180ef890866f63533fe2f8fe5215749
Rodrigo Viannini <87579862+rodrigoviannini@users.noreply.github.com>
2021-07-21 00:59:37 -0300

    incl001

---
+++ 004 - Exercicios Listas.py/001 - Uso Append.py
@@ -0,0 +1,13 @@
+lista = [1, 2, 3]
+
+# print da lista antes do append
+print(lista)
+
+# append
+lista.append(4)
+lista.append("232")
+lista.append(True)
+
+# print da lista após o append
+print(lista)
+
