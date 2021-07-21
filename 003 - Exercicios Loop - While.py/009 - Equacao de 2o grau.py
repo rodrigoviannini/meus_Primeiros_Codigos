c58f076e5939bf7d30c2c223b98a64bfd9a7e54a
Rodrigo Viannini <87579862+rodrigoviannini@users.noreply.github.com>
2021-07-20 23:43:04 -0300

    inclusao ex009

---
+++ 003 - Exercicios Loop - While.py/009 - Equacao de 2o grau.py
@@ -0,0 +1,25 @@
+a = float(input("Digite o valor de a: "))
+
+while a == 0:
+    print("O valor de a não pode ser ZERO!!!")
+    a = float(input("Digite o valor de a: ")) #CASO 1 -> não é uma equação do 2o grau
+    
+# CASO 2 -> é uma equação do 2o grau
+b = float(input("Digite o valor de b: "))
+c = float(input("Digite o valor de c: "))
+
+
+delta = (b**2) - (4 * a * c)
+print(f"O valor de delta é: {delta}")
+
+if delta < 0:
+    print("Raízes complexas, não tem como calcular")
+
+elif delta == 0:
+    x = -b/(2*a)
+    print(f"Raiz única, x = {x}")
+
+else:
+    x1 = (-b + delta**(1/2)) / (2 * a)
+    x2 = (-b - delta**(1/2)) / (2 * a)
+    print(f"Raizes de x1 = {x1} e x2 = {x2}")
