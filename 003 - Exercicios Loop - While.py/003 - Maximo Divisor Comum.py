61f53cf7d297ad757afe905487a3f55e0260e9b6
Rodrigo Viannini <87579862+rodrigoviannini@users.noreply.github.com>
2021-07-20 22:45:38 -0300

    Inclusao de comentários

--- 003 - Exercicios Loop - While.py/003 - Maximo Divisor Comum.py
+++ 003 - Exercicios Loop - While.py/003 - Maximo Divisor Comum.py
@@ -3,19 +3,19 @@
 numA = int(input("Digite um número: "))
 numB = int(input("Digite um número: "))
 
-mdc = 1
+mdc = 1 # Divisão entre dois inteiros sempre é 1
 
 # Preciso achar o maior numero e guardá-lo!
-# divisor -> Garantir que divido pelo menor
+# divisor -> Garantir que será divido pelo menor numero
 
 divisor = numA 
 if numB < numA: 
     divisor = numB
 
 while divisor > 0:
-    if numA % divisor == 0 and numB % divisor == 0:
+    if numA % divisor == 0 and numB % divisor == 0: # Se a divisão for exata por ambos, então encontrei o meu MDC
         mdc = divisor
-        break
-    divisor -= 1
+        break # condição atendida , então interrompo o programa
+    divisor -= 1 # Se não for atingido, vou regredindo e testando
 
 print("MDC", mdc)
