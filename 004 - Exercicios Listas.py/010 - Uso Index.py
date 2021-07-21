b52ba28d11638c4d2ced54217d92d5cce38abd11
Rodrigo Viannini <87579862+rodrigoviannini@users.noreply.github.com>
2021-07-21 14:36:04 -0300

    incl ex010

---
+++ 004 - Exercicios Listas.py/010 - Uso Index.py
@@ -0,0 +1,6 @@
+"""Se quisermos saber qual é a posição (índice) de determinado elemento, usamos o método ".index()"""
+
+# Este método retorna apenas a primeira aparição do elemento:
+
+lista = [34, 2, 5, 8, 8, 8, -34]
+print(lista.index(8))
