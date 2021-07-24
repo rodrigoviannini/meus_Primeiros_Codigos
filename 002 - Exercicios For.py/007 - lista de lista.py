0ca970c69b43d03f39a3ab9cf4aebe70ec4eb952
Rodrigo Viannini <87579862+rodrigoviannini@users.noreply.github.com>
2021-07-24 19:40:34 -0300

    incl 007

---
+++ 002 - Exercicios For.py/007 - lista de lista.py
@@ -0,0 +1,9 @@
+lista = [4, 5, -7, 2, 14, -45, 67, -8, 8, -9, 0, 12]
+listaDobro = []
+
+for elemento in lista:
+    listaDobro.append(2 * elemento)
+print()
+print(lista)
+print()
+print(listaDobro)
